b8940e936f005583f399e3015981dc64011c9452
Tejas Tundulwar <ttejas@rocketmail.com>
2019-02-07 13:41:38 -0800

    Added Code

---
+++ src/Untitled1_mcc_component_data.c
@@ -0,0 +1,172 @@
+/*
+ * MATLAB Compiler: 4.10 (R2009a)
+ * Date: Sat Apr 23 19:59:42 2016
+ * Arguments: "-B" "macro_default" "-o" "Untitled1" "-W" "WinMain:Untitled1"
+ * "-d" "C:\Users\Foxtrot\Documents\Main Project\Color DWT-SVD
+ * watermarking\New\FINAL\Final\Untitled1\src" "-T" "link:exe" "-v"
+ * "C:\Users\Foxtrot\Documents\Main Project\Color DWT-SVD
+ * watermarking\New\FINAL\Final\FINAL.m" "-a" "C:\Users\Foxtrot\Documents\Main
+ * Project\Color DWT-SVD watermarking\New\FINAL\Final\host.jpg" "-a"
+ * "C:\Users\Foxtrot\Documents\Main Project\Color DWT-SVD
+ * watermarking\New\FINAL\Final\host2.jpg" "-a"
+ * "C:\Users\Foxtrot\Documents\Main Project\Color DWT-SVD
+ * watermarking\New\FINAL\Final\watermark.jpg" "-a"
+ * "C:\Users\Foxtrot\Documents\Main Project\Color DWT-SVD
+ * watermarking\New\FINAL\Final\watermark2.jpg" 
+ */
+
+#include "mclmcrrt.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+const unsigned char __MCC_Untitled1_session_key[] = {
+    '9', 'B', 'E', '9', '9', 'E', '5', '9', '9', '3', 'C', '0', '6', 'E', '2',
+    '3', '6', '5', '1', '0', '8', 'F', 'E', '0', '0', '7', 'D', 'D', 'F', '1',
+    '8', '1', '6', '0', 'F', '8', 'B', 'A', '9', 'D', 'A', 'A', 'F', '8', '3',
+    'E', 'C', '3', 'E', '8', 'D', 'C', 'F', '0', '6', '6', '4', 'F', 'C', 'A',
+    'E', 'D', '9', 'A', '3', 'F', 'D', '4', '9', '8', '7', '4', '0', 'F', '7',
+    '4', '4', '8', '0', '0', 'B', '2', '5', 'D', 'F', '6', 'F', '5', '0', '6',
+    'A', '3', 'B', '2', 'A', 'E', 'E', '3', '9', '7', '8', '6', 'B', '1', '4',
+    '2', 'B', 'B', '6', '2', '8', '9', '2', '3', '1', '0', '3', 'F', 'B', 'E',
+    'D', '0', 'C', 'E', '6', '3', '2', 'F', 'F', 'E', 'C', 'E', '3', '8', '3',
+    'C', 'D', 'B', '7', '1', '0', '2', 'E', '9', '3', '6', '5', '7', '3', '1',
+    '8', 'C', '0', '5', 'D', 'C', '3', 'A', '9', 'D', '8', 'A', 'D', '1', '8',
+    'E', '8', '8', '0', '6', '0', '6', 'E', '5', 'F', '3', '0', 'E', 'B', '9',
+    '7', 'E', 'E', '1', 'E', 'F', '6', '3', 'B', 'D', '3', '2', 'F', 'E', '9',
+    '2', 'A', '4', '3', '3', '4', '2', '0', 'B', '4', 'C', '2', '3', '0', '8',
+    'D', '2', '5', 'A', '5', 'A', '6', '0', '3', 'B', 'F', '7', 'D', 'F', 'E',
+    'F', '1', 'F', '1', 'D', '9', '3', 'A', '9', '7', '3', 'F', '6', 'F', '3',
+    '5', 'B', 'D', 'A', 'F', '3', '7', '8', 'D', '9', '7', '1', '9', 'C', '8',
+    'D', '\0'};
+
+const unsigned char __MCC_Untitled1_public_key[] = {
+    '3', '0', '8', '1', '9', 'D', '3', '0', '0', 'D', '0', '6', '0', '9', '2',
+    'A', '8', '6', '4', '8', '8', '6', 'F', '7', '0', 'D', '0', '1', '0', '1',
+    '0', '1', '0', '5', '0', '0', '0', '3', '8', '1', '8', 'B', '0', '0', '3',
+    '0', '8', '1', '8', '7', '0', '2', '8', '1', '8', '1', '0', '0', 'C', '4',
+    '9', 'C', 'A', 'C', '3', '4', 'E', 'D', '1', '3', 'A', '5', '2', '0', '6',
+    '5', '8', 'F', '6', 'F', '8', 'E', '0', '1', '3', '8', 'C', '4', '3', '1',
+    '5', 'B', '4', '3', '1', '5', '2', '7', '7', 'E', 'D', '3', 'F', '7', 'D',
+    'A', 'E', '5', '3', '0', '9', '9', 'D', 'B', '0', '8', 'E', 'E', '5', '8',
+    '9', 'F', '8', '0', '4', 'D', '4', 'B', '9', '8', '1', '3', '2', '6', 'A',
+    '5', '2', 'C', 'C', 'E', '4', '3', '8', '2', 'E', '9', 'F', '2', 'B', '4',
+    'D', '0', '8', '5', 'E', 'B', '9', '5', '0', 'C', '7', 'A', 'B', '1', '2',
+    'E', 'D', 'E', '2', 'D', '4', '1', '2', '9', '7', '8', '2', '0', 'E', '6',
+    '3', '7', '7', 'A', '5', 'F', 'E', 'B', '5', '6', '8', '9', 'D', '4', 'E',
+    '6', '0', '3', '2', 'F', '6', '0', 'C', '4', '3', '0', '7', '4', 'A', '0',
+    '4', 'C', '2', '6', 'A', 'B', '7', '2', 'F', '5', '4', 'B', '5', '1', 'B',
+    'B', '4', '6', '0', '5', '7', '8', '7', '8', '5', 'B', '1', '9', '9', '0',
+    '1', '4', '3', '1', '4', 'A', '6', '5', 'F', '0', '9', '0', 'B', '6', '1',
+    'F', 'C', '2', '0', '1', '6', '9', '4', '5', '3', 'B', '5', '8', 'F', 'C',
+    '8', 'B', 'A', '4', '3', 'E', '6', '7', '7', '6', 'E', 'B', '7', 'E', 'C',
+    'D', '3', '1', '7', '8', 'B', '5', '6', 'A', 'B', '0', 'F', 'A', '0', '6',
+    'D', 'D', '6', '4', '9', '6', '7', 'C', 'B', '1', '4', '9', 'E', '5', '0',
+    '2', '0', '1', '1', '1', '\0'};
+
+static const char * MCC_Untitled1_matlabpath_data[] = 
+  { "Untitled1/", "$TOOLBOXDEPLOYDIR/", "$TOOLBOXMATLABDIR/general/",
+    "$TOOLBOXMATLABDIR/ops/", "$TOOLBOXMATLABDIR/lang/",
+    "$TOOLBOXMATLABDIR/elmat/", "$TOOLBOXMATLABDIR/randfun/",
+    "$TOOLBOXMATLABDIR/elfun/", "$TOOLBOXMATLABDIR/specfun/",
+    "$TOOLBOXMATLABDIR/matfun/", "$TOOLBOXMATLABDIR/datafun/",
+    "$TOOLBOXMATLABDIR/polyfun/", "$TOOLBOXMATLABDIR/funfun/",
+    "$TOOLBOXMATLABDIR/sparfun/", "$TOOLBOXMATLABDIR/scribe/",
+    "$TOOLBOXMATLABDIR/graph2d/", "$TOOLBOXMATLABDIR/graph3d/",
+    "$TOOLBOXMATLABDIR/specgraph/", "$TOOLBOXMATLABDIR/graphics/",
+    "$TOOLBOXMATLABDIR/uitools/", "$TOOLBOXMATLABDIR/strfun/",
+    "$TOOLBOXMATLABDIR/imagesci/", "$TOOLBOXMATLABDIR/iofun/",
+    "$TOOLBOXMATLABDIR/audiovideo/", "$TOOLBOXMATLABDIR/timefun/",
+    "$TOOLBOXMATLABDIR/datatypes/", "$TOOLBOXMATLABDIR/verctrl/",
+    "$TOOLBOXMATLABDIR/codetools/", "$TOOLBOXMATLABDIR/helptools/",
+    "$TOOLBOXMATLABDIR/winfun/", "$TOOLBOXMATLABDIR/winfun/NET/",
+    "$TOOLBOXMATLABDIR/demos/", "$TOOLBOXMATLABDIR/timeseries/",
+    "$TOOLBOXMATLABDIR/hds/", "$TOOLBOXMATLABDIR/guide/",
+    "$TOOLBOXMATLABDIR/plottools/", "toolbox/local/",
+    "toolbox/shared/dastudio/", "$TOOLBOXMATLABDIR/datamanager/",
+    "toolbox/compiler/", "toolbox/images/colorspaces/",
+    "toolbox/images/images/", "toolbox/images/imuitools/",
+    "toolbox/images/iptformats/", "toolbox/images/iptutils/",
+    "toolbox/shared/imageslib/", "toolbox/nnet/nnet/",
+    "toolbox/nnet/nnet/nndistance/", "toolbox/nnet/nnet/nnformat/",
+    "toolbox/nnet/nnet/nnnetinput/", "toolbox/nnet/nnet/nnnetwork/",
+    "toolbox/nnet/nnet/nnperformance/",
+    "toolbox/nnet/nnet/nnprocess/", "toolbox/nnet/nnet/nntopology/",
+    "toolbox/nnet/nnet/nntransfer/", "toolbox/nnet/nnet/nnweight/",
+    "toolbox/nnet/nnutils/", "toolbox/nnet/nnguis/nnguiutils/",
+    "toolbox/wavelet/wavelet/", "toolbox/wavelet/wavedemo/" };
+
+static const char * MCC_Untitled1_classpath_data[] = 
+  { "java/jar/toolbox/images.jar" };
+
+static const char * MCC_Untitled1_libpath_data[] = 
+  { "" };
+
+static const char * MCC_Untitled1_app_opts_data[] = 
+  { "" };
+
+static const char * MCC_Untitled1_run_opts_data[] = 
+  { "" };
+
+static const char * MCC_Untitled1_warning_state_data[] = 
+  { "off:MATLAB:dispatcher:nameConflict" };
+
+
+mclComponentData __MCC_Untitled1_component_data = { 
+
+  /* Public key data */
+  __MCC_Untitled1_public_key,
+
+  /* Component name */
+  "Untitled1",
+
+  /* Component Root */
+  "",
+
+  /* Application key data */
+  __MCC_Untitled1_session_key,
+
+  /* Component's MATLAB Path */
+  MCC_Untitled1_matlabpath_data,
+
+  /* Number of directories in the MATLAB Path */
+  60,
+
+  /* Component's Java class path */
+  MCC_Untitled1_classpath_data,
+  /* Number of directories in the Java class path */
+  1,
+
+  /* Component's load library path (for extra shared libraries) */
+  MCC_Untitled1_libpath_data,
+  /* Number of directories in the load library path */
+  0,
+
+  /* MCR instance-specific runtime options */
+  MCC_Untitled1_app_opts_data,
+  /* Number of MCR instance-specific runtime options */
+  0,
+
+  /* MCR global runtime options */
+  MCC_Untitled1_run_opts_data,
+  /* Number of MCR global runtime options */
+  0,
+  
+  /* Component preferences directory */
+  "Untitled1_15E4408DD72C5ACB89374B147DAA71A7",
+
+  /* MCR warning status data */
+  MCC_Untitled1_warning_state_data,
+  /* Number of MCR warning status modifiers */
+  1,
+
+  /* Path to component - evaluated at runtime */
+  NULL
+
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+
